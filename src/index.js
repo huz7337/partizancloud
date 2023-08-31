4edfde553bc520f661fbb6c2d2b9b212e53e32fa
Volodimir Huz <volodimirhuz@Volodimirs-MacBook-Pro.local>
2023-08-31 16:37:25 +0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import helloWorld from "./scripts/helloWorld";
 import Prism from "prismjs";
 
 import "prismjs/themes/prism-coy.css";
 import "prismjs/components/prism-scss";
 
-helloWorld();
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(function (dropdown) {
+        dropdown.addEventListener('mouseenter', function () {
+            this.classList.add('show');
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            if (dropdownMenu) {
+                dropdownMenu.classList.add('show');
+            }
+        });
+
+        dropdown.addEventListener('mouseleave', function () {
+            this.classList.remove('show');
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            if (dropdownMenu) {
+                dropdownMenu.classList.remove('show');
+            }
+        });
+    });
+});
